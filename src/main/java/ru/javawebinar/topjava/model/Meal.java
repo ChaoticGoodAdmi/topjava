8d373e7ca69eef1b9af494ec3ec4f54cdaa6fab0
ChaoticGoodAdmi <imonare@gmail.com>
2020-10-22 22:58:49 +0300

    HW04: implemented MealJpaRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,77 +1,107 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.All_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_FILTERED, query =
+                "SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime>=:startDate AND m.dateTime<:endDate ORDER BY m.dateTime DESC")
+})
+@Entity
+@Table(name = "meals",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")
+        }
+)
 public class Meal extends AbstractBaseEntity {
+    public static final String DELETE = "Meal.delete";
+    public static final String All_SORTED = "Meal.getAll";
+    public static final String GET_FILTERED = "Meal.getBetweenHalfOpen";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(max = 255)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
-    public Meal() {
+    public Meal () {
     }
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
+    public Meal (LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public Meal (Integer id, LocalDateTime dateTime, String description, int calories) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
-    public LocalDateTime getDateTime() {
+    public LocalDateTime getDateTime () {
         return dateTime;
     }
 
-    public String getDescription() {
+    public String getDescription () {
         return description;
     }
 
-    public int getCalories() {
+    public int getCalories () {
         return calories;
     }
 
-    public LocalDate getDate() {
+    public LocalDate getDate () {
         return dateTime.toLocalDate();
     }
 
-    public LocalTime getTime() {
+    public LocalTime getTime () {
         return dateTime.toLocalTime();
     }
 
-    public void setDateTime(LocalDateTime dateTime) {
+    public void setDateTime (LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 
-    public void setDescription(String description) {
+    public void setDescription (String description) {
         this.description = description;
     }
 
-    public void setCalories(int calories) {
+    public void setCalories (int calories) {
         this.calories = calories;
     }
 
-    public User getUser() {
+    public User getUser () {
         return user;
     }
 
-    public void setUser(User user) {
+    public void setUser (User user) {
         this.user = user;
     }
 
     @Override
-    public String toString() {
+    public String toString () {
         return "Meal{" +
                 "id=" + id +
                 ", dateTime=" + dateTime +
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,65 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Override
-    public Meal save(Meal meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save (Meal meal, int userId) {
+        meal.setUser(em.getReference(User.class, userId));
+        if (meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        } else if (get(meal.getId(), userId) == null) {
+            return null;
+        }
+        return em.merge(meal);
     }
 
     @Override
-    public boolean delete(int id, int userId) {
-        return false;
+    @Transactional
+    public boolean delete (int id, int userId) {
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
-    public Meal get(int id, int userId) {
-        return null;
+    public Meal get (int id, int userId) {
+        Meal meal = em.find(Meal.class, id);
+        return meal == null || meal.getUser().getId() != userId ? null : meal;
     }
 
     @Override
-    public List<Meal> getAll(int userId) {
-        return null;
+    public List<Meal> getAll (int userId) {
+        return em.createNamedQuery(Meal.All_SORTED, Meal.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
-    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+    public List<Meal> getBetweenHalfOpen (LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return em.createNamedQuery(Meal.GET_FILTERED, Meal.class)
+                .setParameter("userId", userId)
+                .setParameter("startDate", startDateTime)
+                .setParameter("endDate", endDateTime)
+                .getResultList();
     }
 }
