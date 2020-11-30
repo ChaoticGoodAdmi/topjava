38814e9d8d7a6fd60c2ac992f314f2388476aa24
ChaoticGoodAdmi <imonare@gmail.com>
2020-11-30 10:21:52 +0300

    fixed updating row instead of table in users.jsp

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -46,14 +46,22 @@ $(function () {
     makeEditable(ctx);
 });
 
-function changeEnabled(userId, value) {
+function changeEnabled(userId, checkbox) {
+    let newValue = checkbox.is(":checked");
     $.ajax({
         type: "POST",
         url: ajaxUrl + userId,
-        data: "value=" + value
+        data: "value=" + newValue
     }).done(() => {
-        $.get(ajaxUrl, updateTable);
+        let rowClassList = checkbox.closest("tr").get(0).classList;
+        if (newValue) {
+            rowClassList.remove("text-muted");
+        } else {
+            rowClassList.add("text-muted");
+        }
         let textMsg = "User " + userId;
-        successNoty(value ? textMsg + " enabled" : textMsg + " disabled");
+        successNoty(newValue ? textMsg + " enabled" : textMsg + " disabled");
+    }).fail(() => {
+        $(checkbox).prop("checked", !newValue);
     });
 }
